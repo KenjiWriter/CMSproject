beb6f3aacb13531a4d12852e81826b970f5e0285
KenjiWriter <46613400+KenjiWriter@users.noreply.github.com>
2020-06-08 18:35:02 +0200

    Add files via upload

---
+++ article.php
@@ -0,0 +1,60 @@
+<?php 
+
+    include_once('includes/connection.php');
+    include_once('includes/article.php');
+	
+	$article = new Article;
+
+	if (isset($_GET['id'])) {
+		
+		$id = $_GET['id'];
+		$data = $article->fetch_data($id);
+		
+		?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
+    <title>Wenzzi Developnet</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <a href="index.php" id="logo"> CMS </a>
+        <h4><?php echo $data['article_title'] ?> -
+            <small>posted <?php echo date('l jS', $data['article_timestamp']); ?>
+            </small></h4>
+
+        <p><?php echo $data['article_content']; ?></p>
+
+
+        <img src='uploads/' <?php.$data['article_image']?> </img>
+
+
+
+
+
+
+        <br> <br>
+        <a href="index.php">&larr; Back</a>
+    </div>
+
+</body>
+
+</html>
+
+
+<?php 
+		
+	} else {
+		header('Location: index.php');
+		exit();
+	}
+	
+
+?>
---
+++ index.php
@@ -0,0 +1,48 @@
+<?php 
+
+    include_once('includes/connection.php');
+    include_once('includes/article.php');
+
+    $article = new Article;
+    $articles = $article->fetch_all();
+	
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
+    <title>Wenzzi Developnet</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <a href="index.php" id="logo"> CMS </a>
+        <ol>
+            <?php foreach ($articles as $article) { ?>
+            <li>
+                <a href="article.php?id=<?php echo $article['article_id']; ?>">
+                    <?php echo $article['article_title']; ?>
+                </a>
+                - <small>
+                    posted <?php echo date('l jS', $article['article_timestamp']); ?>
+                </small>
+            </li>
+            <?php } ?>
+        </ol>
+        <br />
+
+        <small>
+            <a href="admin">
+                Control Panel
+            </a>
+        </small>
+    </div>
+
+</body>
+
+</html>
